37f81d6b25f397e2a99f59d17ff4a0473be1a0e5
Andrew Kondratev <andruhon@gmail.com>
2012-05-02 22:28:17 +1200

    The first commit
    See all previous changes in changelog_EN.txt

---
+++ README.txt
@@ -0,0 +1,32 @@
+kGallery is a gallery written on JavaScript using jQuery library.
+
+Advantage of this gallery is ability to work stable with hundreds of pictures or with big pictures, without freezing brouser. This effect is achieved by retrieving the list of images from external source (JSON or XML) and downloading pictures gradually until slideshow is playing, not all in ones.
+
+Nearest analog is dojox.image.Gallery (dojox.image.ThumbnailPicker+dojox.image.SlideShow), however kGallery do not need to download dozens of JavaScript files with megabytes of code of dojo library.
+
+kGallery includes kSlideshow and kThumbnailPicker componnents, which can used independently.
+
+Examples of usage:
+1.
+2.
+3.
+
+
+Documentation is coming soon. Now you can check all parameters in "defaults" object of every componnent, all variables and methods are well commented.
+
+По русски (in Russian)
+
+kGallery - это галерея написанная на JavaScript с использованием библиотеки jQuery.
+
+Преимущество kGallery заключается в том, что галерея стабильно работает с сотнями изображений, либо с большими изображениями.
+Эффект достигается за счет того, что список изображений и их миниатюр запрашивается из внешнего источника в виде JSON или XML, изображения загружаются не все сразу, а постепенно, по мере воспроизведения слайдшоу.
+
+Ближайший аналог dojox.image.Gallery (dojox.image.ThumbnailPicker+dojox.image.SlideShow), однако kGallery не требует подключения к сайту мегабайтов кода библиотеки dojo и лучше настраивается.
+
+kGallery включает компоненты kSlideshow и kThumbnailPicker, которые могут быть использованы самостоятельно.
+
+Примеры использования:
+http://kopolo.ru/for-developers/kgallery/demo/
+
+Документация:
+http://kopolo.ru/for-developers/kgallery/docs/
---
+++ js/kGallery.js
@@ -0,0 +1,246 @@
+/**
+ * kGallery
+ *
+ * @version 1.04 / 02.05.2012
+ * @author Andrew Kondratev [andr@kopolo.ru]
+ * @requires jQuery JavaScript Library > v1.3.2
+ * @requires kSlideshow > 0.8
+ * @requires kThumbnailPicker > 0.8
+ *
+ * Dual licensed under MIT and GPL 2+ licenses
+ * http://kopolo.ru/for-developers/kgallery/_license/
+ */
+(function($) {
+   kGallery = function(new_options) {
+       
+       kGalleryVersion = '1.04';
+       
+       /* default options */
+       var defaults = {
+               /**
+                * gallery wrapper
+                * If collection of elements given, displays the same slideshow obeying same commands in all wrappers
+                * Call new kSlideshow to create independent slideshow                 * 
+                * var string|$.object
+                */
+               wrapper: '#gallery-wrapper',
+               
+               /**
+                * Url with the data for gallery (JSON usually)
+                * see jQuery.ajax()
+                * var string
+                */
+               url: 'data.json',
+               
+               /**
+                * Data type
+                * see jQuery.ajax()
+                * var string
+                */
+               dataType: 'json',
+               
+               /**
+                * index of initial slide
+                * var integer
+                */
+               startItem: 0,
+               
+               /**
+                * Additional settings for kSlideshow
+                * var object
+                */
+               slideshowOptions: {},
+               
+               /**
+                * Additional settings for kThumbnailPicker
+                * var object
+                */
+               thumbnailPickerOptions: {},
+               
+               /**
+                * CSS class, which adds to gallery wrapper
+                * var string
+                */
+               wrapperClass: 'kGallery',
+                   
+               /**
+                * CSS class, which adds to selected thumbnail
+                * var string
+                */
+               selectedThumbnailClass: 'selectedThumbnail',
+               
+               /**
+                * Function, which calls after initialization
+                * var function
+                */
+               afterInit: function () {},
+               
+               /**
+                * Function, which calls before displaying of picture
+                */
+               beforeShow: function (itemIndex) {}
+       };
+       
+       /**
+        * Link to itself, available anywhere inside class
+        * It can be used like class::this operator even inside of asynchronous methods, which has their own this
+        */
+       var instance = {};
+       
+       /**
+        * Result of megring of user options and default options, available anywhere inside the class
+        * var object
+        * var object
+        */
+       var options;    
+       
+       /**
+        * Array with list of items received from data (see url option)
+        * var array
+        */
+       instance.items = [];
+       
+       /**
+        * jQuery object of wrapper element
+        * var object
+        */
+       instance.wrapper;
+       
+       /**
+        * Object of kSlideshow (with all methods)
+        * var object
+        */
+       instance.slideshow;
+       
+       /**
+        * Object of kThumbnailPicker (with all methods)
+        * var object
+        */
+       instance.thumbnailsPicker;
+       
+       /**
+        * DOM node of last selected thumbnail
+        * var object
+        */
+       instance.lastThumbnail;
+       
+       /**
+        * "Constructor"
+        */
+       {
+           setOptions(new_options);
+           /*
+            * recieveng the data for slideshow
+            */
+           jQuery.ajax({
+               url: options.url,    			
+               success: function (data) {
+                   var items = data.items;
+                   instance.items = items;
+                   instance.init ();
+                   options.afterInit();
+               },
+               dataType: options.dataType
+           });
+       };
+       
+       /**
+        * megring of user options and default options
+        * @param object new_options
+        */
+       function setOptions(new_options) {
+           options = jQuery.extend({}, defaults, options, new_options);
+       };
+       
+       /**
+        * Initialization of gallery
+        */
+       instance.init = function () {
+           instance.wrapper = jQuery(options.wrapper);
+           instance.wrapper.addClass(options.wrapperClass);   		
+
+           instance.initSlideshow(); 
+           instance.initThumbnailPicker();
+       };
+       
+       /**
+        * Initialization of thumbnails
+        */
+       instance.initThumbnailPicker = function () {
+           /* general options for thumbnails */
+           var thumbnailPickerDefaults = {
+                   url: false,
+                   startItem: options.startItem
+           };
+           if (!options.thumbnailPickerOptions.wrapper){               
+               /* Creating thumbnails wrapper if it is not given in options */
+               thumbnailPickerDefaults.wrapper = jQuery('<div></div>').appendTo(instance.wrapper);			
+           };
+           thumbnailPickerDefaults.thumbnailClick = function (itemIndex) {               
+               /* on thumbnail click display in slideshow according picture */
+               if (instance.slideshow && itemIndex != instance.slideshow.currentItemIndex) {
+                   instance.slideshow.showItem(itemIndex);
+               };
+           };
+           thumbnailPickerDefaults.beforeShow = function (nextOrPrev) {
+               /* reset slideshow playback iterval if thumbnails next or prev button is pressed */
+               if (instance.slideshow.isPlay==true) {
+                  instance.slideshow.play();
+               }
+           };
+           
+           /* initialization of preview */
+           var thumbnailPickerOptions = jQuery.extend({},thumbnailPickerDefaults,options.thumbnailPickerOptions);
+           instance.thumbnailPicker = kThumbnailPicker(thumbnailPickerOptions);    		
+           instance.thumbnailPicker.items = instance.items;
+           instance.thumbnailPicker.init();
+           if (instance.thumbnailPicker.items[options.startItem] && instance.thumbnailPicker.items[options.startItem].thumbnailNode) {
+               /* highlighting of initial preview */
+               instance.lastThumbnail = instance.thumbnailPicker.items[options.startItem].thumbnailNode;
+               instance.lastThumbnail.addClass(options.selectedThumbnailClass);
+           };
+       };
+       
+       /**
+        * Initialization of slideshow
+        */
+       instance.initSlideshow = function () {
+           /* general slidesohw options */
+           var slideshowDefaults = {
+                   url: false,
+                   startItem: options.startItem			
+           };    		
+           if (!options.slideshowOptions.wrapper) {
+               /* Creating slideshow wrapper if it is not given in options */
+               slideshowDefaults.wrapper = jQuery('<div></div>').appendTo(instance.wrapper)
+           }; 
+           /* event before changing of picture */
+           slideshowDefaults.beforeShow = function (itemIndex) {
+               options.beforeShow(itemIndex);
+               if (instance.thumbnailPicker) {
+                   if (instance.thumbnailPicker.items[itemIndex].thumbnailNode) {
+                       /* highlightning of selected preview */
+                       instance.thumbnailPicker.items[itemIndex].thumbnailNode.addClass(options.selectedThumbnailClass);
+                       instance.lastThumbnail.removeClass(options.selectedThumbnailClass);
+                       instance.lastThumbnail = instance.thumbnailPicker.items[itemIndex].thumbnailNode;
+                   };
+               };
+           };
+           /* event after changing of picture */
+           slideshowDefaults.afterShow = function (){               
+               /* trying to flip to according page of thumbnails */
+               if (instance.thumbnailPicker) {
+                   instance.thumbnailPicker.showItem(instance.slideshow.currentItemIndex);
+               };
+           };
+           
+           /*  initialization of slideshow */
+           var slideshowOptions = jQuery.extend({},slideshowDefaults,options.slideshowOptions);
+           instance.slideshow = kSlideshow(slideshowOptions);
+           instance.slideshow.items = instance.items;
+           instance.slideshow.init();
+       };
+       
+       return instance;
+   };
+})(jQuery);
